d9413cef57b1efcea3efae3ba2268cb0fee51e6a
m4arthu <luisarthir259@gmail.com>
2023-10-18 23:52:46 -0300

    feat: reserved page

--- src/services/eventApi.js
+++ src/services/eventApi.js
@@ -1,6 +1,7 @@
 import api from './api';
 
 export async function getEventInfo() {
+  console.log(import.meta.env.VITE_API_URL)
   const response = await api.get('/event');
   return response.data;
 }
